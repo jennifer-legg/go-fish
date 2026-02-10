9e39c7a5d2763a1a11e4774d0caab1ce17c90216
Angu Chen <anguchen96@gmail.com>
2026-02-10 15:19:43 +1300

    added some animation to button

--- client/components/themedUI/themedButon.tsx
+++ client/components/themedUI/themedButon.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react'
 import { MouseEventHandler, ReactNode } from 'react'
 
 type buttonColor = 'orange' | 'darkBlue'
@@ -14,12 +15,22 @@ export default function Themedbutton({
   classname = '',
   color = 'orange',
 }: Props) {
+  const transition = {
+    type: 'spring',
+    stiffness: 110,
+    damping: 20,
+    delay: 0.1,
+  } as const
   return (
-    <button
+    <motion.button
+      initial={{ scale: 0, opacity: 0 }}
+      animate={{ scale: 1, opacity: 1 }}
+      transition={transition}
+      whileTap={{ scale: 0.8 }}
       onClick={onClick}
       className={`text-whiteText font-slackey w-44 rounded-full py-2 text-[24px] shadow-md shadow-gray-600 md:w-[368px] md:text-[48px] lg:w-[368px] lg:text-[48px] ${classname} cursor-pointer hover:opacity-80 ${color === 'orange' ? 'bg-fishOrange ' : 'bg-darkBlue'}`}
     >
       {children}
-    </button>
+    </motion.button>
   )
 }
