f511707d22845f56e1d6a126c60eb34b57cbd7a6
Angu Chen <anguchen96@gmail.com>
2026-02-10 15:08:11 +1300

    created and styled themedButton

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ['./index.html', './client/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fishOrange: '#eb6f3a',
+        darkBlue: '#0C0878',
+        whiteText: '#F4F4F4',
+        lightBlue: '#93C8E9',
+      },
+      fontFamily: {
+        slackey: ['Slackey', 'cursive'],
+        garamond: ['"EB Garamond"', 'serif'],
+        lacquer: ['Lacquer', 'system-ui'],
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
