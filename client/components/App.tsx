912e887c2535f0d28da0d9f3c31239e46f7d8b34
Jennifer Legg <jenn.e.legg@gmail.com>
2026-02-11 21:54:12 +1300

    socket.io connecting with basic connection

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,22 +1,49 @@
-import { useGetShuffledDeck } from '../hooks/useDeck.ts'
+// import { useGetShuffledDeck } from '../hooks/useDeck.ts'
 import Themedbutton from './themedUI/themedButon.tsx'
+import { socket, connectSocket, disconnectSocket } from '../../socket.js'
+import { useEffect, useState } from 'react'
 
 function App() {
-  const { data: deck } = useGetShuffledDeck()
+  // const { data: deck } = useGetShuffledDeck()
+  const [isConnected, setIsConnected] = useState(false)
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server')
+      setIsConnected(true)
+    })
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server')
+      setIsConnected(false)
+    })
+
+    // Cleanup on component unmount
+    return () => {
+      socket.off('connect')
+      socket.off('disconnect')
+      disconnectSocket()
+    }
+  }, [])
+
+  const handleConnect = () => {
+    if (isConnected) {
+      disconnectSocket()
+      setIsConnected(false)
+    } else {
+      connectSocket()
+      setIsConnected(true)
+    }
+  }
 
   return (
     <>
       <div className="app">
         <h1 className="text-3xl font-bold underline">Go Fish</h1>
-        <Themedbutton onClick={() => console.log('button clicked')}>
-          Hello
+        <Themedbutton onClick={handleConnect}>
+          {isConnected ? 'Disconnect' : 'Connect'}
         </Themedbutton>
-        {deck && (
-          <p>
-            {deck.cards.length} cards:{' '}
-            {deck.cards.map((card) => `${card.value} ${card.suit}, `)}
-          </p>
-        )}
+        <p>User is {isConnected ? 'connected' : 'disconnected'}</p>
       </div>
     </>
   )
