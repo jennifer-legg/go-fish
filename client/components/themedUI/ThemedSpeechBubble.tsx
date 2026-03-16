4602b2fa858c0a50580903149125ea9be861e39b
Jennifer Legg <jenn.e.legg@gmail.com>
2026-03-17 10:13:57 +1300

    refactor speech bubbles

--- client/components/themedUI/ThemedSpeechBubble.tsx
+++ client/components/themedUI/ThemedSpeechBubble.tsx
@@ -7,32 +7,22 @@ interface Props {
 }
 
 export default function ThemedSpeechBubble({ leftAligned, children }: Props) {
+  const tail = 'w-0 border-fishOrange'
   const leftTail =
-    'w-0 border-r-[20px] border-t-[20px] border-fishOrange border-b-transparent  border-r-transparent'
+    'border-r-[20px] border-t-[20px] border-b-transparent  border-r-transparent'
   const rightTail =
-    'w-0 border-l-[20px] border-b-[20px] border-fishOrange border-t-transparent  border-l-transparent'
+    'border-l-[20px] border-b-[20px] border-t-transparent  border-l-transparent'
   const bubble = 'relative h-32 w-48 rounded-lg bg-fishOrange text-center'
   return (
     <>
       <div
-        className={`flex items-center ${leftAligned ? 'justify-start' : 'justify-end'} m-2 `}
+        className={`m-2 flex items-center ${leftAligned ? 'justify-start' : 'justify-end'}`}
       >
-        {leftAligned && (
-          <>
-            <div className={bubble}>
-              <ThemedText>{children}</ThemedText>
-            </div>
-            <div className={leftTail}></div>
-          </>
-        )}
-        {!leftAligned && (
-          <>
-            <div className={rightTail}></div>
-            <div className={bubble}>
-              <ThemedText>{children}</ThemedText>
-            </div>
-          </>
-        )}
+        {!leftAligned && <div className={`${rightTail} ${tail}`}></div>}
+        <div className={bubble}>
+          <ThemedText>{children}</ThemedText>
+        </div>
+        {leftAligned && <div className={`${leftTail} ${tail}`}></div>}
       </div>
     </>
   )
