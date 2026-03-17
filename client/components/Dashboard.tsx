0c3c86630edcf95d6882745de0c18dadcab289fa
Jennifer Legg <jenn.e.legg@gmail.com>
2026-03-17 13:35:08 +1300

    adding pond to game view

---
+++ client/components/Dashboard.tsx
@@ -0,0 +1,30 @@
+import ThemedContainer from './themedUI/ThemedContainer'
+import ThemedText from './themedUI/ThemedText'
+import { firstHand2Players } from '../../data/deckExample'
+
+export default function Dashboard() {
+  const gameMessage = 'Message goes here'
+  const hand = firstHand2Players
+  return (
+    <ThemedContainer>
+      <ThemedContainer
+        color="darkBlue"
+        classname="w-full h-2/5 md:rounded-t-large md:rounded-b-none"
+      >
+        <ThemedText>{gameMessage}</ThemedText>
+      </ThemedContainer>
+
+      <ThemedContainer vertical={false} classname="w-full pb-[32px] px-[12px]">
+        {hand.map((card) => (
+          <div className="h-auto w-full flex-1" key={card.code}>
+            <img
+              src={card.image}
+              alt={`${card.value} of ${card.suit}`}
+              className="object-cover"
+            />
+          </div>
+        ))}
+      </ThemedContainer>
+    </ThemedContainer>
+  )
+}
