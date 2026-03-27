5ba053eb835073fc1c6dcc6a6df4af02b1a7b873
Jennifer Legg <jenn.e.legg@gmail.com>
2026-03-28 06:23:11 +1300

    adding padding around gameview

--- client/components/GameLayout.tsx
+++ client/components/GameLayout.tsx
@@ -19,30 +19,35 @@ export default function GameLayout({
 }: Props) {
   const columnStruct =
     'grid grid-cols-4 gap-[8px] md:grid-cols-8 md:gap-[8px]  grid-rows-6'
+
   return (
-    <div className={`${columnStruct} h-screen w-full`}>
-      <div id="speech" className="md:col-span-2 md:row-span-4 md:rounded-xl">
-        {chat}
-      </div>
+    <div
+      className={`container mx-auto h-screen md:p-[32px] lg:px-[128px] lg:py-[32px]`}
+    >
+      <div className={`${columnStruct} h-full w-full `}>
+        <div id="speech" className="md:col-span-2 md:row-span-4 md:rounded-xl">
+          {chat}
+        </div>
 
-      <div id="opponent" className=" md:col-span-4">
-        {opponent}
-      </div>
+        <div id="opponent" className=" md:col-span-4">
+          {opponent}
+        </div>
 
-      <div id="score" className="md:col-span-2">
-        {score}
-      </div>
+        <div id="score" className="md:col-span-2">
+          {score}
+        </div>
 
-      <div id="pond" className=" md:col-span-6 md:row-span-2">
-        {pond}
-      </div>
+        <div id="pond" className=" md:col-span-6 md:row-span-2">
+          {pond}
+        </div>
 
-      <div id="dashboard" className=" md:col-span-6 md:row-span-3">
-        {dashboard}
-      </div>
+        <div id="dashboard" className=" md:col-span-6 md:row-span-3">
+          {dashboard}
+        </div>
 
-      <div id="avatarUser" className=" md:col-span-2 md:row-span-2">
-        {avatarUser}
+        <div id="avatarUser" className=" md:col-span-2 md:row-span-2">
+          {avatarUser}
+        </div>
       </div>
     </div>
   )
